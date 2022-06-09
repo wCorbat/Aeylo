284dce883bef29b76fba1e07d2ab642640e08d34
Walison Corbat <walisoncorbat@gmail.com>
2022-06-09 13:31:52 -0300

    Adição de efeito reveal

--- script.js
+++ script.js
@@ -9,15 +9,21 @@ counter = 1;
 }
 }, 6000);
 
-/* Reveal 
+$(window).on("load scroll", function(){
+    var threshold = 30; // porcentagem da altura visível do elemento que aparecer na tela para iniciar a animação
+     $(".mostrar").each(function(){
+         var el = $(this);
+         var eleHeight = el.outerHeight(); // altura da div
+         var eleTopo = el.offset().top; // distancia da div ao topo do documento
+         var scrlTopo = $(window).scrollTop(); // quanto foi rolada a janela
+         var distance = eleTopo-scrlTopo; // distancia da div ao topo da janela
+         var altJanela = window.innerHeight; // altura da janela
+         if(distance <= altJanela-(eleHeight/(threshold/10))) {
+             el.animate({ 'opacity': 1 }, 1200); // 1200 = 1,2 segundo da animação. Quanto menor, mais rápido
+         }
+     });
+ });
 
-window.sr = ScrollReveal({ reset: true });
-
-sr.reveal('nav', { duration: 1000 });
-
-sr.reveal('.slides, .destaques_container, .abstrato_container, .anime_container, .cartoon_container, .manga_container, .realista_container, .pintura_container, .tresd_container, .planos_container, .title-section-cards, .titulo-fim-de-secao',  { duration: 1000 });
-
-*/
 
 /* Menu Hamburguer Mobile */
 
